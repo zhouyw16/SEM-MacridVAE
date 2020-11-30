8aa91fcfafe61c37dd22cd760859286cdc8257b2
周煜威 <zhouyw16@mails.tsinghua.edu.cn>
2020-11-30 18:42:13 +0800

    need update jointly

--- model.py
+++ model.py
@@ -20,6 +20,15 @@ def load_net(model, N, M, K, D, tau, dropout, items, cores):
 
 
 
+def recon_loss(inputs, logits):
+    return torch.mean(torch.sum(-logits * inputs, dim=1))
+
+def kl_loss(mu, logvar):
+    return torch.mean(0.5 * torch.sum(
+        torch.exp(logvar) + mu ** 2 - 1 - logvar, dim=1))
+
+
+
 class MultiDAE(nn.Module):
     def __init__(self, M, D, dropout):
         super(MultiDAE, self).__init__()
@@ -63,8 +72,7 @@ class MultiDAE(nn.Module):
 
     def loss_fn(self, X, X_logits, X_mu, X_logvar,
                 A, A_logits, A_mu, A_logvar, anneal):
-        recon_loss = torch.mean(torch.sum(-X_logits * X, dim=1))
-        return recon_loss
+        return recon_loss(X, X_logits)
     
 
 
@@ -122,9 +130,7 @@ class MultiVAE(nn.Module):
     
     def loss_fn(self, X, X_logits, X_mu, X_logvar,
                 A, A_logits, A_mu, A_logvar, anneal):
-        recon_loss = torch.mean(torch.sum(-X_logits * X, dim=1))
-        kl_loss = torch.mean(0.5 * torch.sum(torch.exp(X_logvar) + X_mu ** 2 - 1 - X_logvar, dim=1))
-        return recon_loss + anneal * kl_loss
+        return recon_loss(X, X_logits) + anneal * kl_loss(X_mu, X_logvar)
 
 
 
@@ -200,9 +206,7 @@ class DisenVAE(nn.Module):
 
     def loss_fn(self, X, X_logits, X_mu, X_logvar,
                 A, A_logits, A_mu, A_logvar, anneal):
-        recon_loss = torch.mean(torch.sum(-X_logits * X, dim=1))
-        kl_loss = torch.mean(0.5 * torch.sum(torch.exp(X_logvar) + X_mu ** 2 - 1 - X_logvar, dim=1))
-        return recon_loss + anneal * kl_loss
+        return recon_loss(X, X_logits) + anneal * kl_loss(X_mu, X_logvar)
 
 
 
@@ -248,15 +252,22 @@ class DisenSE(nn.Module):
         init.xavier_normal_(self.A_cores)
 
 
-    def cluster(self):
-        items = F.normalize(self.items, dim=1)
-        cores = F.normalize(self.cores, dim=1)
+    def X_cluster(self):
+        items = F.normalize(self.X_items, dim=1)
+        cores = F.normalize(self.X_cores, dim=1)
         cates = torch.mm(items, cores.t()) / self.tau
         cates = F.softmax(cates, dim=1)
         return items, cores, cates
 
+    def A_cluster(self):
+        users = F.normalize(self.A_users, dim=1)
+        cores = F.normalize(self.A_cores, dim=1)
+        cates = torch.mm(users, cores.t()) / self.tau
+        cates = F.softmax(cates, dim=1)
+        return users, cores, cates
 
-    def encode(self, X, cates):
+
+    def X_encode(self, X, cates):
         n = X.shape[0]
         X = X.view(n, 1, self.M) *  \
             cates.t().expand(n, self.K, self.M)     # n * K * M
@@ -265,14 +276,34 @@ class DisenSE(nn.Module):
         mu, logvar = h[:, :self.D], h[:, self.D:]   # (n * k) * D
         return mu, logvar
 
+    def A_encode(self, A, cates):
+        n = A.shape[0]
+        A = A.view(n, 1, self.N) *  \
+            cates.t().expand(n, self.K, self.N)     # n * K * N
+        A = A.reshape(n * self.K, self.N)           # (n * K) * N
+        h = self.encoder(A)                         # (n * K) * D * 2
+        mu, logvar = h[:, :self.D], h[:, self.D:]   # (n * k) * D
+        return mu, logvar
 
-    def decode(self, z, items, cates):
+
+    def X_decode(self, z, items, cates):
         n = z.shape[0] // self.K
         z = F.normalize(z, dim=1)                   # (n * K) * D
         logits = torch.mm(z, items.t()) / self.tau  # (n * K) * M
         probs = torch.exp(logits)                   # (n * K) * M
         probs = torch.sum(probs.view(n, self.K, self.M) * \
                 cates.t().expand(n, self.K, self.M), dim=1)
+        logits = torch.log(probs)
+        logits = F.log_softmax(logits, dim=1)
+        return logits
+
+    def A_decode(self, z, items, cates):
+        n = z.shape[0] // self.K
+        z = F.normalize(z, dim=1)                   # (n * K) * D
+        logits = torch.mm(z, items.t()) / self.tau  # (n * K) * M
+        probs = torch.exp(logits)                   # (n * K) * M
+        probs = torch.sum(probs.view(n, self.K, self.N) * \
+                cates.t().expand(n, self.K, self.N), dim=1)
         logits = torch.log(probs)
         logits = F.log_softmax(logits, dim=1)
         return logits
@@ -288,18 +319,25 @@ class DisenSE(nn.Module):
 
 
     def forward(self, X, A):
-        X_items = F.normalize(self.X_items, dim=1)
-        X_cores = F.normalize(self.X_cores, dim=1)
-        X_cates = torch.mm(X_items, X_cores.t()) / self.tau
-        X_cates = F.softmax(X_cates, dim=1)
+        X_items, _, X_cates = self.X_cluster()
+        A_items, _, A_cates = self.A_cluster()
+        X_mu, X_logvar = self.X_encode(X, X_cates)
+        A_mu, A_logvar = self.A_encode(A, A_cates)
+        X_z = self.sample(X_mu, X_logvar)
+        A_z = self.sample(A_mu, A_logvar)
 
-        A_users = F.normalize(self.A_users, dim=1)
-        A_cores = F.normalize(self.A_cores, dim=1)
-        A_cates = torch.mm(A_users, A_cores.t()) / self.tau
-        A_cates = F.softmax(A_cates, dim=1)
+        # update X_z and A_z jointly
+        X_z = X_z + A_z # * TODO
+        A_z = A_z + X_z # * TODO
 
-        return logits, mu, logvar, None, None, None
+        X_logits = self.X_decode(X_z, X_items, X_cates)
+        A_logits = self.A_decode(A_z, A_items, A_cates)
+
+        return X_logits, X_mu, X_logvar, A_logits, A_mu, A_logvar
     
-    def loss_fn(self, X, X_recon, X_mu, X_logvar,
-                A, A_recon, A_mu, A_logvar, anneal):
-        return
+    def loss_fn(self, X, X_logits, X_mu, X_logvar,
+                A, A_logits, A_mu, A_logvar, anneal):
+        return recon_loss(X, X_logits) + anneal * kl_loss(X_mu, X_logvar) \
+             + recon_loss(A, A_logits) + anneal * kl_loss(A_mu, A_logvar)
+
+
