210fa11e8672c9fd59521aa614f545d74092a7c5
周煜威 <zhouyw16@mails.tsinghua.edu.cn>
2020-12-09 14:25:20 +0800

    delete

--- data/ml-1m/prep.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-# read
-ratings = pd.read_csv('ratings.dat', sep='::', header=None, engine='python',
-                      usecols=[0,1,2], names=['user', 'item', 'rating'])
-
-# write
-ratings.to_csv('prep/ratings.txt', index=False)
--- data/ml-20m/prep.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-# read
-ratings = pd.read_csv('ratings.csv', sep=',', header=0,
-                      usecols=[0,1,2], names=['user', 'item', 'rating'])
-
-# write
-ratings.to_csv('prep/ratings.txt', index=False)
